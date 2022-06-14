9f4464875b7f9091f3f63c41f9819f0723a0b882
Paula Lopes <pauladev21@gmail.com>
2022-06-14 20:44:45 -0300

    refactoring of buildCards code to use polimorfismo with functions

--- assets/js/memoryUI.js
+++ assets/js/memoryUI.js
@@ -2,6 +2,100 @@
 
 // const QUESTIONS_FILE = './assets/data/questions.txt';
 
+function shuffleCards(nCards) {
+    const SHUFFLE_ROUNDS = 100;
+    let generateIndexes = i => [...Array(i).keys()];
+    let indexes = generateIndexes(nCards);
+    let aux;
+
+    for (let round = 0; round < SHUFFLE_ROUNDS; round++) {
+        for (let i = 0; i < nCards; i++) {
+            indexToChange = Math.floor(Math.random() * nCards);
+
+            aux = indexes[i];
+            indexes[i] = indexes[indexToChange];
+            indexes[indexToChange] = aux;
+        }
+    }
+    return indexes;
+}
+
+//===========================INTERFACE CONSTRUCTION============================
+
+
+function buildCardTop(index) {
+    let cardTop = document.createElement('img');
+    cardTop.setAttribute('src', CARD_TOP_IMAGE);
+    cardTop.setAttribute('class', 'card-top');
+    cardTop.setAttribute('alt', 'Click the card to see the content.');
+    cardTop.setAttribute('id', 'card_' + index);
+    return cardTop;
+}
+
+function buildQuestionCardFace(pairId) {
+    let cardFace = prepareCardFace();
+    cardFace.setAttribute('id', 'question_' + pairId);
+    cardFace.innerHTML = MOCK_QUESTIONS[Math.floor(pairId / 2)].question;
+    return cardFace;
+}
+
+function buildAnswerCardFace(pairId) {
+    let cardFace = prepareCardFace();
+    cardFace.setAttribute('id', 'answer_' + pairId);
+    cardFace.innerHTML = MOCK_QUESTIONS[Math.floor(pairId / 2)].answer;
+    return cardFace;
+}
+
+function prepareCardFace() {
+    let cardFace = document.createElement('div');
+    cardFace.setAttribute('class', 'card-face');
+    cardFace.setAttribute('alt', 'Search for the card which matches this.');
+    return cardFace
+}
+
+
+function buildCardWrapper(pairId, faceBuilder) {
+    let cardTop = buildCardTop(pairId);
+    let cardFace = faceBuilder(pairId);
+    let cardWrapper = document.createElement('div');
+
+    cardWrapper.setAttribute('class', 'card-wrapper');
+    cardWrapper.appendChild(cardTop);
+    cardWrapper.appendChild(cardFace);
+
+    return cardWrapper;
+}
+
+function createCards(questions) {
+
+    let cardWrappers = [];
+
+    for (let i = 0; i < questions.length; i++) {
+
+        cardWrappers.push(buildCardWrapper(i, buildQuestionCardFace));
+        cardWrappers.push(buildCardWrapper(i, buildAnswerCardFace));
+    }
+    return cardWrappers;
+}
+
+function resetBoard() {
+    let cardWrappers = cards;
+    let boardElement = memoryBoard;
+    let sInd = shuffleCards(cardWrappers.length);
+
+    let randomCard, cardIndex;
+
+    for (let i = 0; i < cardWrappers.length; i++) {
+        cardIndex = sInd[i]
+        randomCard = cardWrappers[cardIndex];
+        boardElement.appendChild(randomCard);
+    }
+    return boardElement;
+}
+
+
+// ===========================INITIALIZING INTERFACE=====================================
+
 let MOCK_QUESTIONS = [
     {
         question: "What for JavaScript use DOM?",
@@ -55,82 +149,7 @@ const CARD_TOP_IMAGE = "./assets/img/card-top.jpg";
 const cards = createCards(MOCK_QUESTIONS);
 const memoryBoard = document.getElementById('memory-board')
 
-document.getElementsByTagName('body')[0].setAttribute('onload', resetBoard(cards, memoryBoard));
-
-document.getElementById('reset-btn').setAttribute('active', resetBoard(cards, memoryBoard));
-
-function shuffleCards(nCards) {
-    const SHUFFLE_ROUNDS = 100;
-    let generateIndexes = i => [...Array(i).keys()];
-    let indexes = generateIndexes(nCards);
-    let aux;
-
-    for (let round = 0; round < SHUFFLE_ROUNDS; round++) {
-        for (let i = 0; i < nCards; i++) {
-            indexToChange = Math.floor(Math.random() * nCards);
-
-            aux = indexes[i];
-            indexes[i] = indexes[indexToChange];
-            indexes[indexToChange] = aux;
-        }
-    }
-    return indexes;
-}
-
-//===========================INTERFACE CONSTRUCTION============================
-
-
-function buildCardTop(index) {
-    let cardTop = document.createElement('img');
-    cardTop.setAttribute('src', CARD_TOP_IMAGE);
-    cardTop.setAttribute('class', 'card-top');
-    cardTop.setAttribute('id', 'card_' + index);
-    return cardTop;
-}
-
-function buildCardFace(index, stringType) {
-    let cardFace = document.createElement('div');
-    cardFace.setAttribute('class', 'card-face');
-    cardFace.setAttribute('id', 'face_' + index);
-    cardFace.innerHTML = MOCK_QUESTIONS[Math.floor(index/2)][stringType];
-    return cardFace;
-}
-
-function buildCardWrapper(index, attributeName) {
-    let cardTop = buildCardTop(index);
-    let cardFace = buildCardFace(index, attributeName);
-    let cardWrapper = document.createElement('div');
-
-    cardWrapper.setAttribute('class', 'card-wrapper');
-    cardWrapper.appendChild(cardTop);
-    cardWrapper.appendChild(cardFace);
-
-    return cardWrapper;
-}
-
-function createCards(questions) {
-
-    let cardWrappers = [];
-
-    for (let i = 0; i < questions.length * 2; i += 2) {
-        
-        cardWrappers.push(buildCardWrapper(i, 'question'));
-        cardWrappers.push(buildCardWrapper(i+1, 'answer'));
-    }
-    return cardWrappers;
-}
-
-function resetBoard(cardWrappers, boardElement) {
-    let sInd = shuffleCards(cardWrappers.length);
-
-    let randomCard, cardIndex;
-
-    for (let i = 0; i < cardWrappers.length; i++) {
-        cardIndex = sInd[i]
-        randomCard = cardWrappers[cardIndex];
-        boardElement.appendChild(randomCard);
-    }
-    return boardElement;
-}
+document.getElementById('memory-wrapper').setAttribute('onload', resetBoard());
+document.getElementById('reset-btn').setAttribute('onclick', resetBoard());
 
 
